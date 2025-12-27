e760a7d664c813d62da179974dbbc700dc037746
kwesithedev <174883738+KwesitheDev@users.noreply.github.com>
2025-12-27 00:47:39 +0000

    feat: changed Home to OnBoarding, setHeader known to false added DasboardScreen to Stack

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ import HomeScreen from "./src/screens/HomeScreen";
 import { AuthProvider } from "./src/context/AuthContext";
 import LoginScreen from "./src/screens/LoginScreen";
 import RegisterScreen from "./src/screens/RegisterScreen";
+import DashboardScreen from "./src/screens/DashboardScreen";
 
 const Stack = createNativeStackNavigator();
 
@@ -13,10 +14,14 @@ export default function App() {
   return (
     <AuthProvider>
       <NavigationContainer>
-        <Stack.Navigator initialRouteName="Home">
-          <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Navigator
+          initialRouteName="OnBoarding"
+          screenOptions={{ headerShown: false }}
+        >
+          <Stack.Screen name="OnBoarding" component={HomeScreen} />
           <Stack.Screen name="Login" component={LoginScreen} />
           <Stack.Screen name="Register" component={RegisterScreen} />
+          <Stack.Screen name="Dashboard" component={DashboardScreen} />
         </Stack.Navigator>
         <StatusBar style="auto" />
       </NavigationContainer>
